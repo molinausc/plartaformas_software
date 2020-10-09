1ed2d4597083148a5cd34f03e7f8040f1743bc5f
robertoir <roberto.iglesias.rodriguez@usc.es>
2020-10-09 21:34:27 +0200

    finalizado el 12

--- b1e12_turtle_logica/scripts/publicador.py
+++ b1e12_turtle_logica/scripts/publicador.py
@@ -2,8 +2,8 @@
 
 import rospy
 import math
-from std_msgs.msg import Int32 , String
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
 def callback(msg):
     global pub
@@ -26,38 +26,61 @@ def callback(msg):
     angMin = math.degrees(radsMin)
 
     """ Toma de decisiones """
-    """
-    a. Si la distancia en frente del robot es mayor que 1 metro, el robot se debe mover
-        hacia adelante
-    b. Si la distancia en frente del robot es menor que 1 metro, el robot debe girar a la
-        izquierda
-    c. Si la distancia en la parte derecha del robot es menor que 1 metro, el robot
-        girará hacia la izquierda
-    d. Si la distancia en la parte izquierda del robot es menor que 1 metro, el robot
-        girará hacia la derecha.robo
-    """
-  
-    dFrontal = 0 #TODO: obtenerlo de alguna manera
-    dDerecha = 0
-    dIzquierda = 0
+
+    # Voy a partir en tres partes el rango de medidas del laser y lo utilizare para identificar cada zona del robot (frontal, izquierda y derecha)    
+    partes = len(rangos)/3
+
+    # Minima distancia en cada zona
+    dDerecha = maxRango 
+    dFrontal = maxRango 
+    dIzquierda = maxRango
+    for i in range(0,len(rangos)):
+        if i < math.floor(partes): 
+            if rangos[i] < dDerecha:
+                dDerecha = rangos[i]
+        elif i < math.floor(partes)*2:
+            if rangos[i] < dFrontal:
+                dFrontal = rangos[i]
+        else:
+            if rangos[i] < dIzquierda:
+                dIzquierda = rangos[i]
+
+    #Creamos el objeto del request
+    twist = Twist()
+    twist.linear.x = 0.0
+    twist.linear.y = 0.0
+    twist.linear.z = 0.0
+    twist.angular.x = 0.0
+    twist.angular.y = 0.0
+    twist.angular.z = 0.0
 
     if dFrontal > 1:
         # avanzo frontal
-    elif dFrontal < 1:
+        twist.linear.x = 0.5
+    elif dFrontal < 1: 
         # giro izquierda
+        twist.angular.z = 2*math.pi/4 
     elif dDerecha < 1:
         # giro izquierda
+        twist.angular.z = 2*math.pi/4
     elif dIzquierda < 1:
         # giro derecha
+        twist.angular.z = -2*math.pi/4
+
+    """ 
+    No avanzo en el giro porque no se especifica concretamente y presupongo que es por usar distintas opciones de ROS 
+    Ademas publico de 1 en 1 basandome en la frecuencia con la que recibo las mediciones de scan. Por eso no uso el Rate()
+    """
         
+    pub.publish(twist)
 
 
 if  __name__ == '__main__':
 
     rospy.init_node('b1e11_publi_arco',anonymous=True) 
-    pub = rospy.Publisher("b1e11_publi_arco", String, queue_size=1)
+    pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=1)
         
     rospy.Subscriber("/scan", LaserScan, callback)
     rospy.spin()
 
-    
+
