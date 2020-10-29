b225ebbfa822defe7c25c78acebce699f3c9decd
robertoir <roberto.iglesias.rodriguez@usc.es>
2020-10-29 12:01:50 +0100

    terminado b2e4

--- boletin2/b2e4_serv_bas/scripts/service.py
+++ boletin2/b2e4_serv_bas/scripts/service.py
@@ -7,8 +7,18 @@ from b2e4_serv_bas.srv import RespuestaServicio, RespuestaServicioResponse
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
+def resetGlobalVars():
+    global subs, tInicio, tiempoEntrada, velEntrada, stop, tiempoRecorrido, continua
+    subs = None
+    tInicio = 0
+    tiempoEntrada = 0
+    velEntrada = 0
+    stop = False
+    tiempoRecorrido = 0
+    continua = True
+
 def callbackSus(msg):
-    global pub
+    global pub, subs, velEntrada, tiempoEntrada, stop, tInicio, tiempoRecorrido, continua
     laserScan = msg 
     minAng = laserScan.angle_min
     maxAng = laserScan.angle_max
@@ -56,39 +66,79 @@ def callbackSus(msg):
     twist.angular.y = 0.0
     twist.angular.z = 0.0
 
-    # Solo comtemplo la distancia frontal, no las laterales
-    if dFrontal > 1:
-        # avanzo frontal
-        twist.linear.x = 0.5
+    # comprobamos el tiempo pasado
+    now = time.time()
+    diff = now - tInicio
+    cont = True
+    if diff > tiempoEntrada:
+        subs.unregister()
+        stop = True
+        subs = None
+    else:
+        # Solo comtemplo la distancia frontal, no las laterales
+        if dFrontal > 1:
+            # avanzo frontal
+            twist.linear.x = velEntrada
+        else: 
+            subs.unregister()
+            stop = True 
+            cont = False
+            subs = None
 
+    tiempoRecorrido = diff
+    continua = cont
+        
     pub.publish(twist)
 
+    # para controlar las frecuencias por consola y con el tiempo ver que se desuscribe y para de consumir recursos
+    print("PRINT FROM SUSCRIBER",now) 
+
 def callback(request):
-    global yaSuscrito, tiempoEntrada, velEntrada
+    global subs, tiempoEntrada, velEntrada, stop, tInicio, tiempoRecorrido, continua
+
+    # para poder seguir haciendo llamadas al servicio sin tener que reiniciarlo
+    resetGlobalVars()
+
     print("Parametros recibidos:")
     tiempoEntrada = int(request.tiempoEntrada)
     velEntrada = float(request.velEntrada)
     print("Tiempo:",tiempoEntrada)
     print("Velocidad:",velEntrada)
 
-    if not yaSuscrito:
-        yaSuscrito = True
-        # nos suscribimos al topic del scaner
-        res = rospy.Subscriber("/scan", LaserScan, callbackSus)
-
+    tInicio = time.time()
 
     tret = RespuestaServicioResponse()
-    tret.continua = True
-    tret.tiempoMovimiento = 23
+    tret.continua = False
+    tret.tiempoMovimiento = 0
+
+    # comprobamos que la velocidad no sea muy alta
+    if velEntrada >= 0.11 or velEntrada <= -0.11: # aproximo a 0.11 porque si introduces 0.1 te da algun decimal mas y da problemas
+        return tret
+
+    # comprobamos que no esta instanciado ya el suscriber
+    if subs == None:
+        # nos suscribimos al topic del scaner
+        subs = rospy.Subscriber("/scan", LaserScan, callbackSus) 
+
+    rate = rospy.Rate(1)
+    while not stop:
+        # para conctrolar las frecuencias por consola y ver que se ejecuta en paralelo al callback del suscriptor
+        print("PRINT FROM SERVICE") 
+        rate.sleep()
+
+    tret.continua = continua
+    tret.tiempoMovimiento = tiempoRecorrido
 
     return tret
 
-yaSuscrito = False
-tInicio = time.time()
+subs = None
+tInicio = 0
 tiempoEntrada = 0
 velEntrada = 0
+stop = False
+tiempoRecorrido = 0
+continua = True
 rospy.init_node("b2e4_serv_bas")
-rate = rospy.Rate(0.5)
 # lanzamos el servicio
 servicio = rospy.Service("b2e4_serv_bas",RespuestaServicio,callback)
 # preparamos para publicar la velocidad
